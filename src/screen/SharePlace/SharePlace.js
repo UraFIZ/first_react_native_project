90ad2ac1b1de70c15ea43fa56b22a42adaf6ff7e
UraFIZ <kratser88@gmail.com>
2019-02-11 01:55:26 +0200

    git get set up and instoled

---
+++ src/screen/SharePlace/SharePlace.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class SharePlace extends Component {
+  render() {
+    return (
+      <View>
+        <Text> SharePlace </Text>
+      </View>
+    );
+  }
+}
